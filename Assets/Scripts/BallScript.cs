df03d9eec27ebe62afbc6bf86264ef25962e3c5e
Kanabuhochi <arko212@gmail.com>
2018-05-05 10:37:13 +0200

    First script
    Ball, adn bounadaries

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public CircleCollider2D cc;
+    public float ballForce;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb.AddForce(new Vector2(ballForce, ballForce));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Slider")
+        {
+                this.cc.sharedMaterial = null;
+                this.rb.gravityScale = 1;
+        }
+    }
+}
